9599ffa161c1f3115cd182efe6f9d76ad0405d57
Ivolian <ivolianer2014@163.com>
2014-10-28 13:46:09 +0800

    New changelist

--- test/cn/net/withub/busiGate/ezy/service/FormTest.java
+++ test/cn/net/withub/busiGate/ezy/service/FormTest.java
@@ -12,21 +12,34 @@ import java.util.Map;
 
 public class FormTest extends BaseJunit4Test {
 
-    @Resource(name = "req_instanceview_service")
-    private BusiGateService instanceViewService;
+    @Resource(name = "req_miscellaneousview_service")
+    private BusiGateService miscellaneousViewService;
 
-    @Resource(name = "req_taskpending_service")
-    private BusiGateService taskPendingService;
+    @Resource(name = "req_trainingview_service")
+    private BusiGateService trainingViewService;
+
+    @Resource(name = "req_leaveview_service")
+    private BusiGateService leaveViewService;
+
+    @Resource(name = "req_outgoingview_service")
+    private BusiGateService outgoingViewService;
+
+    @Resource(name = "req_caruseview_service")
+    private BusiGateService carUseViewService;
 
     @Test
-    public void testInstanceView() {
+    public void testMiscellaneousView() {
 
         Map params = new HashMap();
-        params.put("objectId", "0815A172-05F3-4875-9348-D80403A50733");
+        params.put("objectId", "04E1B60E-E71C-4BDF-BA62-7A6395532412");
         User loginInfo = new User();
         loginInfo.setUserId("053E0687-EF24-4E46-91BE-DA65A198F001");
         try {
-            Map result = instanceViewService.busi(params, loginInfo);
+            Map result = miscellaneousViewService.busi(params, loginInfo);
+
+            System.out.println("===================================================");
+            System.out.println(result.toString());
+            System.out.println("===================================================");
 
             Assert.assertNotNull(result.get("result"));
 
@@ -36,19 +49,21 @@ public class FormTest extends BaseJunit4Test {
         }
     }
 
-
     @Test
-    public void testTaskPending() {
+    public void testTrainingView() {
 
         Map params = new HashMap();
-        params.put("currentPage", "1");
-        params.put("pageSize", "5");
+        params.put("objectId", "742CE584-71EE-426B-A55F-4350AB1FEAF9");
         User loginInfo = new User();
         loginInfo.setUserId("053E0687-EF24-4E46-91BE-DA65A198F001");
         try {
-            Map result = taskPendingService.busi(params, loginInfo);
+            Map result = trainingViewService.busi(params, loginInfo);
 
-            Assert.assertNotNull(result.get("count"));
+            System.out.println("===================================================");
+            System.out.println(result.toString());
+            System.out.println("===================================================");
+
+            Assert.assertNotNull(result.get("result"));
 
         } catch (AppException e) {
             e.printStackTrace();
@@ -56,5 +71,70 @@ public class FormTest extends BaseJunit4Test {
         }
     }
 
+    @Test
+    public void testLeaveView() {
+
+        Map params = new HashMap();
+        params.put("objectId", "15D5DBE1-4F69-45CE-9C11-A091C2CAA5B3");
+        User loginInfo = new User();
+        loginInfo.setUserId("053E0687-EF24-4E46-91BE-DA65A198F001");
+        try {
+            Map result = leaveViewService.busi(params, loginInfo);
+
+            System.out.println("===================================================");
+            System.out.println(result.toString());
+            System.out.println("===================================================");
+
+            Assert.assertNotNull(result.get("result"));
+
+        } catch (AppException e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void outgoingLeaveView() {
+
+        Map params = new HashMap();
+        params.put("objectId", "1B66A231-F24C-4536-AFDB-CD8511EF43A8");
+        User loginInfo = new User();
+        loginInfo.setUserId("053E0687-EF24-4E46-91BE-DA65A198F001");
+        try {
+            Map result = outgoingViewService.busi(params, loginInfo);
+
+            System.out.println("===================================================");
+            System.out.println(result.toString());
+            System.out.println("===================================================");
+
+            Assert.assertNotNull(result.get("result"));
+
+        } catch (AppException e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void carUseLeaveView() {
+
+        Map params = new HashMap();
+        params.put("objectId", "46ADED4A-6A15-4235-8267-CEBEFE4E5D46");
+        User loginInfo = new User();
+        loginInfo.setUserId("053E0687-EF24-4E46-91BE-DA65A198F001");
+        try {
+            Map result = carUseViewService.busi(params, loginInfo);
+
+            System.out.println("===================================================");
+            System.out.println(result.toString());
+            System.out.println("===================================================");
+
+            Assert.assertNotNull(result.get("result"));
+
+        } catch (AppException e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
 
 }
